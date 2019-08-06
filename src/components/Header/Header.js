90a3060634e414cc76dadf91414478329e933bd9
ntoneko <nicolastonelli1406@gmail.com>
2019-08-06 13:27:42 -0300

    Info tooltip fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,34 +10,36 @@ const Header = () => {
     <div className="header-container">
       <h2>Horarios de El Villarino - 319</h2>
       <OutsideClick action={() => setShowInfo(false)}>
-        <button
-          className="info-btn"
-          onClick={() => setShowInfo(() => !showInfo)}
-        >
-          <InfoIcon />
-        </button>
-        <CSSTransition
-          in={showInfo}
-          timeout={400}
-          classNames="info"
-          unmountOnExit
-          appear
-          onEntered={() => setShowInfo(true)}
-          onExit={() => setShowInfo(false)}
-        >
-          <div className="info-modal">
-            <strong>ACLARACION:</strong> La información utilizada en esta app no
-            corresponde a información en tiempo real sino que proviene de{" "}
-            <a
-              className="villarino-link"
-              href="http://www.elvillarino.com.ar/#horarios"
-            >
-              aquí
-            </a>{" "}
-            . Por lo que la precisión dependerá de la regularidad de las
-            unidades y de la exactitud con la funcionan.
-          </div>
-        </CSSTransition>
+        <>
+          <button
+            className="info-btn"
+            onClick={() => setShowInfo(() => !showInfo)}
+          >
+            <InfoIcon />
+          </button>
+          <CSSTransition
+            in={showInfo}
+            timeout={400}
+            classNames="info"
+            unmountOnExit
+            appear
+            onEntered={() => setShowInfo(true)}
+            onExit={() => setShowInfo(false)}
+          >
+            <div className="info-modal">
+              <strong>ACLARACION:</strong> La información utilizada en esta app
+              no corresponde a información en tiempo real sino que proviene de{" "}
+              <a
+                className="villarino-link"
+                href="http://www.elvillarino.com.ar/#horarios"
+              >
+                aquí
+              </a>{" "}
+              . Por lo que la precisión dependerá de la regularidad de las
+              unidades y de la exactitud con la funcionan.
+            </div>
+          </CSSTransition>
+        </>
       </OutsideClick>
     </div>
   );
