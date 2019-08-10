2615837a6f9bc7539e5aa1bdf7e712071e79f669
ntoneko <nicolastonelli1406@gmail.com>
2019-08-10 12:53:57 -0300

    Info modal content & style (shadow) update

--- src/components/Header/Info/Info.js
+++ src/components/Header/Info/Info.js
@@ -54,6 +54,22 @@ const Info = () => {
           onExit={() => setShowInfo(false)}
         >
           <div className="info-modal">
+            <h3>Funcionamiento</h3>
+            <p>
+              <strong>"Desde:"</strong> Indica la parada por la que pasará la
+              unidad en ese horario.
+            </p>
+            <p>
+              <strong>"Hacia:"</strong> Indica el sentido del recorrido.
+            </p>
+            <p>
+              <strong>Ejemplo:</strong> Si usted se encuentra en Bahía Blanca y
+              desea ir hacia Punta Alta deberá seleccionar la parada en la que
+              se encuentra. Para eso debe seleccionar una de las paradas que se
+              encuentran en el listado <strong>"Desde"</strong>. Por último debe
+              elegir <strong>"Hacia"</strong> donde se dirige, en este caso,
+              Punta Alta.
+            </p>
             <strong>ACLARACION:</strong> La información utilizada en esta app no
             corresponde a información en tiempo real sino que proviene de{" "}
             <a
