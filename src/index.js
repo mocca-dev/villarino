61c0edf49fe3a6aa7bd41af1f8e18fe841917f65
Damelanote2501 <nicolastonelli1406@gmail.com>
2024-06-03 00:42:30 -0300

    Update for working with node18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const sWPromise = serviceWorker.register();
 
-ReactDOM.render(<App sWPromise={sWPromise} />, document.getElementById("root"));
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App sWPromise={sWPromise} />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
